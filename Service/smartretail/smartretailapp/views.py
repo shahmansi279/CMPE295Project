b786e81a5355af3bb12071881b0e9cabcca6609a
jessiedeot <jessiedeot@gmail.com>
2015-12-22 21:29:20 -0800

    Product Filter

--- Service/smartretail/smartretailapp/views.py
+++ Service/smartretail/smartretailapp/views.py
@@ -28,6 +28,20 @@ class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset=Product.objects.all()
     serializer_class=ProductSerializer
 
+
+class ProductFilter(generics.ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+
+        queryset = Product.objects.all()
+        category = self.request.query_params.get('category', None)
+        if category is not None:
+            queryset = queryset.filter(product_category_id=category)
+        return queryset
+
+
+
 def home(request):
 
     return HttpResponse("Hello World")
