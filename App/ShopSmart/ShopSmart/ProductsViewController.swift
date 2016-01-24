0e5ba946fe0b482602575bc417efec0a46843f5f
shahmansi279 <shah.mansi279@gmail.com>
2016-01-24 11:21:10 -0800

    Changes to screens

--- App/ShopSmart/ShopSmart/ProductsViewController.swift
+++ App/ShopSmart/ShopSmart/ProductsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ProductsViewController: UICollectionViewController {
+class ProductsViewController :UIViewController {
 
     
     
@@ -50,32 +50,7 @@ class ProductsViewController: UICollectionViewController {
     
     
     
-    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
-    
-        return classArr.count;
-        
-    }
-    
-    // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
-        
-       
-        
-        
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier( "cat", forIndexPath: indexPath)
-        
-        // "\(classArr[indexPath.row])"
-        
-        return cell
-    
-    }
-    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int
-    {
-        
-        return 1;
-    }
-    
-    /*
+      /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
