637f141f404d6df390b1fa50275ad55a9264251e
shahmansi279 <shah.mansi279@gmail.com>
2016-02-22 18:11:43 -0800

    Add CollectionViewCell

---
+++ App/ShopSmart/ShopSmart/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  ShopSmart
+//
+//  Created by Mansi Modi on 2/22/16.
+//  Copyright © 2016 Mansi Modi. All rights reserved.
+
+
+
+class CollectionViewCell: UICollectionViewCell {
+    
+ 
+
+    @IBOutlet weak var imgView: UIImageView!
+
+}
