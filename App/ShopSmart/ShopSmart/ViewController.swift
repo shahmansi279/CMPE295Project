deb9a91151120d1e09e3c4f0795a392751e503e0
shahmansi279 <shah.mansi279@gmail.com>
2016-03-08 13:14:34 -0800

    Offer Changes for Location

--- App/ShopSmart/ShopSmart/ViewController.swift
+++ App/ShopSmart/ShopSmart/ViewController.swift
@@ -12,10 +12,13 @@ import Alamofire
 
 
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout,CLLocationManagerDelegate {
     
     var data: [Offer] = []
-   
+    
+    var location: NSString?
+    var manager:CLLocationManager!
+    var userLocationZipCode:NSString?
     
     @IBOutlet var collectionView: UICollectionView!
     @IBOutlet var Menu: UIBarButtonItem!{
@@ -41,28 +44,157 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         // Do any additional setup after loading the view, typically from a nib.
     
         
+        //Fetch the location authorization
+        requestLocationAuthorization()
+
+        
+        
         //Fetch the data from Smart Retail Service
         loadData()
         
         
     }
     
+    /* Request Location Authorization from the User */
     
-    func loadData(){
+    func requestLocationAuthorization(){
+        
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+    }
     
-        Alamofire.request(.GET, "http://127.0.0.1:8000/smartretailapp/api/offer/?format=json")
-            .responseJSON {  response in
-                switch response.result {
-                case .Success(let JSON):
-                    self.populateData(JSON as! NSMutableArray)
-                    
-                                        
-                case .Failure(let error):
-                    print("Request failed with error: \(error)")
-                   
-                }
+    /* Check Location Authorization Status */
+    
+    func checkLocationServiceStatus() -> Bool {
+        
+        
+        if CLLocationManager.locationServicesEnabled() {
+            
+            switch(CLLocationManager.authorizationStatus()) {
+                
+            case .NotDetermined, .Restricted, .Denied:
+                return false
+                
+            case .AuthorizedAlways, .AuthorizedWhenInUse:
+                
+                return true
+                
+            default:
+                return false
+            }
+        }
+        else {
+            
+            return false
         }
     }
+    
+    /*On Location Authorization Status Event Change */
+    
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        
+        if(checkLocationServiceStatus()){
+            //If user allows access to location services
+            print ("Location service enabled")
+            manager.desiredAccuracy = kCLLocationAccuracyBest
+            manager.startUpdatingLocation()}
+            
+        else {
+            
+            //if user does not allow access
+            print ("Location service disabled")
+            
+            
+        }
+        
+    }
+    
+
+       /* fetch the user location */
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error) -> Void in
+            if (error != nil) {
+                print("Reverse geocoder failed with error" + error!.localizedDescription)
+                return
+            }
+            
+            if (placemarks!.count > 0 ){
+                let pm = placemarks![0] as CLPlacemark
+                self.displayLocationInfo(pm)
+            } else {
+                print("Problem with the data received from geocoder")
+            }
+        })
+    }
+    
+    
+    func displayLocationInfo(placemark: CLPlacemark!) {
+        if placemark != nil {
+            //stop updating location to save battery life
+            manager.stopUpdatingLocation()
+            self.userLocationZipCode = placemark.postalCode
+            
+            /*print(placemark.locality)
+            print(placemark.postalCode)
+            print(placemark.administrativeArea)
+            print(placemark.country)
+            print (placemark.subLocality)
+            print (placemark.areasOfInterest)*/
+            
+           
+        }
+        
+    }
+
+    
+    
+    func loadData(){
+        
+        if(self.userLocationZipCode != nil)
+    
+        {
+            
+            
+            Alamofire.request(.GET, "http://127.0.0.1:8000/smartretailapp/api/offernearby/?format=json")
+                .responseJSON {  response in
+                    switch response.result {
+                    case .Success(let JSON):
+                        self.populateData(JSON as! NSMutableArray)
+                        
+                        
+                    case .Failure(let error):
+                        print("Request failed with error: \(error)")
+                        
+                    }
+            }
+
+            
+        }
+        
+        else{
+        
+        
+            Alamofire.request(.GET, "http://127.0.0.1:8000/smartretailapp/api/offer/?format=json")
+                .responseJSON {  response in
+                    switch response.result {
+                    case .Success(let JSON):
+                        self.populateData(JSON as! NSMutableArray)
+                        
+                        
+                    case .Failure(let error):
+                        print("Request failed with error: \(error)")
+                        
+                    }
+            }
+
+        
+        }
+    }
+    
     
     
     func populateData(jsonData: NSMutableArray){
@@ -118,8 +250,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ImageCell", forIndexPath: indexPath) as! CollectionViewCell
         
-        cell.contentView.frame = cell.bounds
-        cell.contentView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        //cell.contentView.frame = cell.bounds
+        //cell.contentView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
         
         let url = NSURL(string: "\(self.data[indexPath.row].offerImgUrl)")
         
