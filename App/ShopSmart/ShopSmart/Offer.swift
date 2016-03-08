6335b8d0242426ccded687a1c25de07baea2d25a
shahmansi279 <shah.mansi279@gmail.com>
2016-03-08 12:15:17 -0800

    Commit changes for offer

--- App/ShopSmart/ShopSmart/Offer.swift
+++ App/ShopSmart/ShopSmart/Offer.swift
@@ -21,9 +21,9 @@ struct Offer {
     
 
     init(data : NSDictionary){
-        offerId = data["offer_id"] as! Int
-        offerDesc = data["offer_desc"] as! String
-        offerTitle = data["offer_title"] as! String
+        offerId = data["offer_id"] as? Int
+        offerDesc = data["offer_desc"] as? String
+        offerTitle = data["offer_title"] as? String
       //offerExpiry = (data["offer_end_date"] as! NSDate)
         offerImgUrl =  data["offer_img_url"] as! String
     }
