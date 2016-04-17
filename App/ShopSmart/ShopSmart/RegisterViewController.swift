148fa6bacc7337f28b5abd9b2c8b5abe1f4fe9be
jessiedeot <jessiedeot@gmail.com>
2016-04-16 20:24:44 -0700

    Keyboard Dismiss

--- App/ShopSmart/ShopSmart/RegisterViewController.swift
+++ App/ShopSmart/ShopSmart/RegisterViewController.swift
@@ -112,8 +112,18 @@ class RegisterViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(RegisterViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
 
         // Do any additional setup after loading the view.
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
 
     override func didReceiveMemoryWarning() {
